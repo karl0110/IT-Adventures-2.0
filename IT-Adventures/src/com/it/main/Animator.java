e21056c5ada3b9a255288044cc82a14d57033289
karl0110 <pjaimehall@gmail.com>
2017-06-21 00:27:51 +0200

    comments

--- IT-Adventures/src/com/it/main/Animator.java
+++ IT-Adventures/src/com/it/main/Animator.java
@@ -2,23 +2,21 @@ package com.it.main;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-
 /**
- * 
+ * Die Klasse ist für das animieren des Spielers zuständig, welches durch das abspielen von verschiedenen Bildern geschiet.
  * 
  * @author RealTutsGML
- *
  */
 public class Animator {
 
-	private BufferedImage[] images;
+	private BufferedImage[] images;//alle Bilder, welche abgespielt werden sollen.
 	private BufferedImage currentImage;
 	
-	private int tickSpeed;
-	private int frames;
+	private int tickSpeed;//Wie schnell die Bilder abgespielt werden sollen.
+	private int frames;//Wie viele Bilder es gibt.
 	
-	private int tickCount=0;
-	private int count=0;
+	private int tickCount=0;//Wie oft das Animator-Objekt aktualisiert wurde, wird als Timer genutzt.
+	private int count=0;//Die aktuelle Position im Array, also das aktuelle Bild
 	
 
 	public Animator(BufferedImage[] images, int tickSpeed) {
@@ -29,9 +27,12 @@ public class Animator {
 
 	}
 
+	/**
+	 * Die Animation wird aktualisiert.
+	 */
 	public void runAnimation(){
 		tickCount++;
-		if(tickCount > tickSpeed){
+		if(tickCount > tickSpeed){//Falls die Zeit vorübergeht, welche zwischen Bildern festgelegt wurde.
 			tickCount=0;
 			nextFrame();
 		}
@@ -39,7 +40,7 @@ public class Animator {
 	
 	private void nextFrame(){
 		for(int i =0;i<frames;i++){
-			if(count==i){
+			if(count==i){//Das neue Bild wird aus dem Array geladen
 				currentImage=images[i];
 			}
 		}
@@ -49,7 +50,10 @@ public class Animator {
 			count=0;
 		}
 	}
-	
+	/**
+	 * 
+	 * Die Grafik der Aniamtion wird gemalt.
+	 */
 	public void renderAnimation(Graphics g,float x,float y,float width, float height) {
 		g.drawImage(currentImage, (int)x,(int) y,(int) width,(int)height,null);
 	}
