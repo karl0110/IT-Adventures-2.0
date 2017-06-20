4b19cd540547d3d02a079cb66817d9eba7af833c
Vince <Vince@192.168.1.3>
2017-06-21 01:25:28 +0200

    block comment

--- IT-Adventures/src/com/it/main/Block.java
+++ IT-Adventures/src/com/it/main/Block.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
  * 
  * @author Vincent, Karl
  *
+ * Klasse zur Spezialisierung der Erstellung aller Blöcke im Spiel; Spezialisierung: Kollision, Aussehen.
  */
 public class Block extends TileObject{
 
@@ -19,13 +20,13 @@ public class Block extends TileObject{
 
 	public void render(Graphics g) {
 
-		if(player.getX()>x-Game.WIDTH &&player.getX()<x+Game.WIDTH){
+		if(player.getX()>x-Game.WIDTH &&player.getX()<x+Game.WIDTH){ // Nur die auf dem Bildschirm erkennbaren Blöcke werden geladen
 			g.drawImage(image, (int)x, (int)y, (int)width, (int)height, null);
 		}
 		
 	}
 
-	
+	// Alle Kollisionsrechtecke(-methoden) eines Objekts "Block" für die geregelte Kollision zwischen ihm und anderen Spielobjekten
 	public Rectangle getUpperBounds() {
 		return new Rectangle((int)x,(int)y,(int)width,(int)height);
 	}
