f9412eb21a9e75f39c4afa93fce980de9db46c62
karl0110 <pjaimehall@gmail.com>
2017-06-21 01:25:48 +0200

    comments

--- IT-Adventures/src/com/it/main/TileHandler.java
+++ IT-Adventures/src/com/it/main/TileHandler.java
@@ -4,10 +4,21 @@ package com.it.main;
 import java.awt.Graphics;
 import java.util.LinkedList;
 
+/**
+ * Das Objekt dieser Klasse beeinhaltet alle Objekte des aktuellen Levels in
+ * einer Liste. Von diesen werden dann alle Grafiken gemalt und alle Objekte die
+ * dies benötigen, werden aktualisiert.
+ * 
+ * @author Jaime Hall
+ *
+ */
 public class TileHandler {
 
-	public LinkedList<Tile> object = new LinkedList<Tile>();
-	public LinkedList<TileEntity> tickObject = new LinkedList<TileEntity>();
+	public LinkedList<Tile> object = new LinkedList<Tile>();// Alle Spielobjekte
+	public LinkedList<TileEntity> tickObject = new LinkedList<TileEntity>();// Alle
+																			// zu
+																			// aktualisierenden
+																			// Objekte
 
 	public void tick() {
 		for (int i = 0; i < tickObject.size(); i++) {
@@ -26,22 +37,36 @@ public class TileHandler {
 		}
 	}
 
+	/**
+	 * Methode fügt den Listen ein neues Objekt hinzu.
+	 * 
+	 * @param object
+	 *            Das neue Spielobjekt
+	 */
 	public void addObject(Tile object) {
 		this.object.add(object);
-		if(object.isTileEntity()){
-			this.tickObject.add((TileEntity)object);
+		if (object.isTileEntity()) {
+			this.tickObject.add((TileEntity) object);
 		}
 	}
 
+	/**
+	 * Methode entfernt ein Objekt aus den Listen.
+	 * 
+	 * @param object Das zu entfernende Objekt.
+	 */
 	public void removeObject(Tile object) {
 		this.object.remove(object);
-		if(object.isTileEntity()){
-			this.tickObject.remove((TileEntity)object);
+		if (object.isTileEntity()) {
+			this.tickObject.remove((TileEntity) object);
 		}
 	}
 
+	/**
+	 * Entfernt alle Objekte aus den Listen.
+	 */
 	public void removeAllObjects() {
 		object = new LinkedList<Tile>();
-		tickObject= new LinkedList<TileEntity>();
+		tickObject = new LinkedList<TileEntity>();
 	}
 }
